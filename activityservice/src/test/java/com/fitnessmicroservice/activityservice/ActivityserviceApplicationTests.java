aae7bc935dea09c6935c31479895153bef0bf0cb
Elhassene Ba <elhasseneba00@gmail.com>
2025-12-19 18:45:34 +0000

    first commit for the initial activity service

--- activityservice/src/test/java/com/fitnessmicroservice/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/fitnessmicroservice/activityservice/ActivityserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ActivityserviceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
